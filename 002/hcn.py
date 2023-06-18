38a09654373abc9c5197b0e802ed8f4d1468e903
vanquyen0912 <hoangvanquyen091207@gmai.com>
2023-06-18 10:33:30 +0700

    noi dung

---
+++ 002/hcn.py
@@ -0,0 +1,25 @@
+# B1: Nhúng thư viện
+import turtle
+# B2 Tạo đối tượng
+t= turtle.Turtle()
+# B3: Vẽ ngang
+t.forward(100)
+# B4: quay phải 90 độ
+t.right(90)
+# b5: vẽ ngang
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.penup()
+t.goto(-150,0)
+t.pendown()
+t.forward(200)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(100)
+turtle.done()
