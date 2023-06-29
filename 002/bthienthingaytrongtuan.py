d4132df7385a4f90f2335d78da28bfa2a5cfeb02
vanquyen0912 <hoangvanquyen091207@gmai.com>
2023-06-29 18:54:26 +0700

    noi dung

---
+++ 002/bthienthingaytrongtuan.py
@@ -0,0 +1,17 @@
+A = int(input("Nhap so A :"))
+if A == 1:
+    print("Monday")
+elif A == 2:
+    print("Tuesday")
+elif A == 3:
+    print("Wednesday")
+elif A == 4:
+    print("Thursday")
+elif A == 5:
+    print("Friday")
+elif A == 6:
+    print("Saturday")
+elif A == 7:
+    print("Sunday")
+else:
+    print("error, out of range")
